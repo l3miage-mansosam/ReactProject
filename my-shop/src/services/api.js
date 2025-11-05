3ed475e18f143bf8c5f30f8ff9c93a878bf1b805
sami.mansour <mansoursami98@gmail.com>
2025-11-05 20:44:12 +0100

    test deploy

--- my-shop/src/services/api.js
+++ my-shop/src/services/api.js
@@ -1,5 +1,5 @@
 //const API_BASE_URL = 'https://react-project-ochre-theta.vercel.app/api';
-const API_BASE_URL= 'http://localhost:3000/api';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const getAuthHeaders = () => {
   const token = localStorage.getItem('authToken');
